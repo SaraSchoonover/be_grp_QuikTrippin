35a484bf02c3b1e055e6ba93f8901fecbf91f54e
Sara Schoonover <saramschoon@gmail.com>
2021-08-09 20:48:58 -0500

    Add project files.

---
+++ be_grp_QuikTrippin/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace be_grp_QuikTrippin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userEntry = 0;
+            // print menu
+            // validate menu
+            
+
+            switch (userEntry)
+            {
+                case 1:
+                    // enter district sales
+                    // print menu
+                    // validate menu
+                    break;
+                case 2:
+                    // generate district report
+                    // print menu
+                    // validate menu
+                    break;
+                case 3:
+                    // Add New Employee
+                    // print menu
+                    // validate menu
+                    break;
+                case 4:
+                    // Add a Store/District
+                    // print menu
+                    // validate menu
+                    break;
+                case 5:
+                    // exit
+                default:
+                    break;
+            }
+        }
+    }
+}
